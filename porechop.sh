94d4fdaeadea60edff51accb6302da9406f7f612
Muskan-Sachdeva <sachdevamuskan666@gmail.com>
2025-12-30 20:40:20 +0530

    Create porechop.sh

---
+++ porechop.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# The sa,e script used for all the genotypes
+# Script: porechop_ont_adapter_trimming.sh
+# Purpose: Adapter trimming and chimera removal for Oxford Nanopore (ONT) long-read data
+# Tool: Porechop
+#
+# Input  : Raw ONT FASTQ file
+# Output : Adapter-trimmed FASTQ file suitable for genome assembly
+#
+# Notes:
+# - Porechop trims sequencing adapters from ONT reads
+# - --discard_middle removes chimeric reads containing internal adapters
+# - This script is suitable for long-read genome assembly workflows
+#
+
+# -------------------------------
+# Input FASTQ file (ONT reads)
+# -------------------------------
+INPUT_FASTQ="/tmp/muskan/Fastq/SO_12802_NSPP_3C_barcode13.fastq"
+
+# -------------------------------
+# Output FASTQ file (trimmed reads)
+# -------------------------------
+OUTPUT_FASTQ="/tmp/muskan/porechop/NSPP_3C_trimmed.fastq"
+
+# -------------------------------
+# Run Porechop
+# -------------------------------
+porechop \
+  -i ${INPUT_FASTQ} \
+  -o ${OUTPUT_FASTQ} \
+  --threads 32 \
+  --adapter_threshold 95 \
+  --discard_middle
