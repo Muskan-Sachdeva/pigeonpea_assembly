0caaa82561972d93b2cb107352c17a2620ecee11
Muskan-Sachdeva <sachdevamuskan666@gmail.com>
2026-08-04 12:51:19 +0530

    Update porechop.sh

--- porechop.sh
+++ porechop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# The sa,e script used for all the genotypes
+# The same script used for all the genotypes
 # Script: porechop_ont_adapter_trimming.sh
 # Purpose: Adapter trimming and chimera removal for Oxford Nanopore (ONT) long-read data
 # Tool: Porechop
-#
-# Input  : Raw ONT FASTQ file
-# Output : Adapter-trimmed FASTQ file suitable for genome assembly
+#Author: MUSKAN
+# Input: Raw ONT FASTQ file
+# Output: Adapter-trimmed FASTQ file suitable for genome assembly
 #
 # Notes:
 # - Porechop trims sequencing adapters from ONT reads
